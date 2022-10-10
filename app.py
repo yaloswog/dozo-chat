33f99e1f8292cfa9fb8d2b5816c99cc7bcb69232
yaloswog <yaloswog@gmail.com>
2022-10-09 21:49:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
 conversation = client.conversations \
                      .v1 \
                      .conversations \
-                     .create(friendly_name='My First Conversation')
+                     .create(friendly_name='dozo-chat')
 
 print(conversation.sid)
